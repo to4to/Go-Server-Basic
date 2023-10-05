fa7991fa85d2ba3bcc13f9e681dc10311f3e46eb
to4to <justjustifying@outlook.com>
2023-10-05 22:48:43 +0530

    Adding Routes

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
 	fileerver := http.FileServer(http.Dir("./static"))
-	http.Handle()
+	http.Handle("/", fileerver)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello"helloHandler)
+
+	fmt.Println("Server Started")
 }
