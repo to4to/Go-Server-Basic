53744dbf01fa0926bb31f451adef2f2e2e0f8df5
to4to <justjustifying@outlook.com>
2023-10-06 21:30:35 +0530

    Completed

--- main.go
+++ main.go
@@ -2,15 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Valid", http.StatusNotFound)
+	}
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parse Form() err: %v", err)
+
+	}
+	fmt.Fprintf(w, "Post Request Successful ")
+	name := r.FormValue("name ")
+	address := r.FormValue("address ")
+
+	fmt.Fprintf(w, "name =%s", name)
+	fmt.Fprintf(
+		w,
+		"address is %s",
+		address,
+	)
+}
 func main() {
 
-	fileerver := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileerver)
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
 	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello"helloHandler)
+	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Println("Server Started")
+	fmt.Println("Server Started at port 8080")
+
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
